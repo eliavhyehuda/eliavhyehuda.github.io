9a4306fb976ca8024ca91f6a4319eaf1615ccae9
eliav yehuda <eyehuda@uwaterloo.ca>
2021-01-10 13:31:18 +0200

    Added new resume and some minor fixes

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -6,6 +6,21 @@ $(function() {
 	
 });
 
+$(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+
 // smoothScroll function is applied from the document ready function
 function smoothScroll (duration) {
 	$('a[href^="#"]').on('click', function(event) {
